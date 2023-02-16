77f2a392a155dd95bd751f4b977e8853a065d265
Sameer Ahmed <sameerahmed@Sameers-MacBook-Air.local>
2023-02-16 19:54:35 -0330

    Returning k value list in sorting function

--- solution.py
+++ solution.py
@@ -1,4 +1,6 @@
 def partial_flip(number_list, k):
+    # TODO: try to optimize this function
+
     sub_array = [0 for i in range(k)]
     for i in range(k):
         sub_array[i] = number_list[i]
@@ -7,37 +9,41 @@ def partial_flip(number_list, k):
 
     for i in range(k):
         number_list[i] = flipped_array[i]
-
-    # for i in number_list:
-    #     print(i)
     
     return number_list
 
 
 def sorting(number_list):
+    # TODO: try to optimize this function
+
     max_num = max(number_list)
     min_num = min(number_list)
     
     last = len(number_list)
 
+    k_list = []
+
     for i in range(len(number_list)):
         for j in range(len(number_list)):
-            if number_list[j] == max_num:
+            if max_num == 1:
+                break
+            elif number_list[j] == max_num:
                 number_list =  partial_flip(number_list, j+1)
+                k_list.append(j+1)
                 max_num = max_num - 1
                 number_list = partial_flip(number_list, last)
+                k_list.append(last)
                 last = last - 1
-                for x  in number_list:
-                    print(x)
+                
+    for i in k_list:
+        print(i)
 
+    return k_list
     
-
-          
-
 
 sorting([3,2,4,1])
 
-#Strategy
+#Strategy 1
 #[3,2,4,1] min = 1, max = 4
 #[1,4,2,3] min = 2, max = 4
 #[4,1,2,3] min = 2, max = 3
@@ -47,7 +53,7 @@ sorting([3,2,4,1])
 #max is at first index implies flip from last
 #[1,2,3,4]
 
-#Strategy #2
+#Strategy 2
 #Flip from the maximum number index. Bring max num to index 0
 #then flip from last element so that the max is at the end of list
 #Decrement max and last everytime they are used as index for flipping
