61dfd65f67ed64fff80f361a880e046ff9fc943b
Sameer Ahmed <sameerahmed@Sameers-MacBook-Air.local>
2023-02-18 12:48:14 -0330

    Changing directory structure

--- test_solution.py
+++ test_solution.py
@@ -1,16 +1,16 @@
 import unittest
 
-import partialflipsort
+import src.partial_flip_sort as partial_flip_sort
 
 class TestFlipSort(unittest.TestCase):
 
     def test_partial_flip(self):
-        self.assertEqual(partialflipsort.partial_flip([3,2,4,1], 4), [1,4,2,3])
-        self.assertEqual(partialflipsort.partial_flip([3,2,4,1], 3), [4,2,3,1])
+        self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 4), [1,4,2,3])
+        self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 3), [4,2,3,1])
 
     def test_sorting(self):
-        self.assertEqual(partialflipsort.sorting([3,2,4,1]), [3,4,2,3,1,2])
-        self.assertEqual(partialflipsort.sorting([3,2,4,1,7,5,6]), [5,7,1,6,5,5,2,4,1,3,2,2])
+        self.assertEqual(partial_flip_sort.sorting([3,2,4,1]), [3,4,2,3,1,2])
+        self.assertEqual(partial_flip_sort.sorting([3,2,4,1,7,5,6]), [5,7,1,6,5,5,2,4,1,3,2,2])
 
 
 if __name__ == '__main__':
