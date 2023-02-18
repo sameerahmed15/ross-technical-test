04e460e580527848ce528e98ebaed3eab0e9569e
Sameer Ahmed <sameerahmed@Sameers-MacBook-Air.local>
2023-02-18 12:03:34 -0330

    Code refactoring

--- partialflipsort.py
+++ partialflipsort.py
@@ -1,4 +1,13 @@
 def partial_flip(arr, k):
+    """Reverse a sub-array from given index
+
+        Parameters
+        ----------
+        arr: list
+            Array of integers from 1 to len(arr)-1
+        k : int
+            Index value from where the array will be reversed
+    """
 
     arr[0:k] = arr[k-1::-1]
     
@@ -6,10 +15,16 @@ def partial_flip(arr, k):
 
 
 def sorting(arr):
-    # TODO: try to optimize this function
+    """Sort a list of integers from smallest to highest
+
+        Parameters
+        ----------
+        arr: list
+            Array of integers from 1 to len(arr)-1
+    """
 
     len_arr = len(arr)
-    max_num = max(arr)
+    max_num = len_arr
     last = len_arr
     ks = []
     for i in range(len_arr):
@@ -17,11 +32,16 @@ def sorting(arr):
             if max_num == 1:
                 break
             elif arr[j] == max_num:
+                # Shift max to index 0
                 arr =  partial_flip(arr, j+1)
                 ks.append(j+1)
                 max_num -= 1
+                # Shift max to last index
                 arr = partial_flip(arr, last)
                 ks.append(last)
                 last -= 1
+                break
 
     return ks
+
+sorting([3,2,4,1])
