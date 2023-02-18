9a3937f07371b3fa041046cda76fd224d5649e68
Sameer Ahmed <sameerahmed@Sameers-MacBook-Air.local>
2023-02-18 13:14:27 -0330

    Add more tests

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -8,10 +8,14 @@ class TestFlipSort(unittest.TestCase):
     def test_partial_flip(self):
         self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 4), [1,4,2,3])
         self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 3), [4,2,3,1])
+        self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 1), [3,2,4,1])
+        self.assertEqual(partial_flip_sort.partial_flip([3,2,4,1], 2), [2,3,4,1])
 
     def test_sorting(self):
         self.assertEqual(partial_flip_sort.sorting([3,2,4,1]), [3,4,2,3,1,2])
         self.assertEqual(partial_flip_sort.sorting([3,2,4,1,7,5,6]), [5,7,1,6,5,5,2,4,1,3,2,2])
+        self.assertEqual(partial_flip_sort.sorting([4,2,3,5,1,6]), [6,6,4,5,2,4,1,3,2,2])
+        self.assertEqual(partial_flip_sort.sorting([3,1,2]), [1,3,1,2])
 
 
 if __name__ == '__main__':
